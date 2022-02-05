8502b45cb4a0a384ffd352c5fc0c98eca6691419
Russell Soza <endosoza1@gmail.com>
2022-02-05 15:10:21 -0600

    Tailwind añadido al proyecto

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      transitionDuration: {
+        '25': '25ms'
+      }
+    },
+  },
+  plugins: [],
+}
